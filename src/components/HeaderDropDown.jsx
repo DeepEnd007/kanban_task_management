ad32553e8be8b4689285b6e8cdbbf7e206d00663
DeepEnd007 <yihuihu56@gmail.com>
2024-04-10 00:16:45 +1200

    AddEditBoradModal

--- src/components/HeaderDropDown.jsx
+++ src/components/HeaderDropDown.jsx
@@ -36,7 +36,7 @@ function HeaderDropDown({ setOpenDropdown }) {
         <div>
           {boards.map((board, index) => (
             <div
-              className={` flex items-baseline space-x-2 px-5 py-4  ${
+              className={` flex items-baseline  dark:text-white space-x-2 px-5 py-4  ${
                 board.isActive &&
                 " bg-[#635fc7] rounded-r-full text-white mr-8 "
               } `}
---
+++ src/modals/AddEditBoardModal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddEditBoardModal() {
+  return <div>AddEditBoardModal</div>;
+}
+
+export default AddEditBoardModal;
