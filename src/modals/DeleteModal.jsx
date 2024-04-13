d6a96c90dda3b955019c1bd08ecaf4f4c7073657
DeepEnd007 <yihuihu56@gmail.com>
2024-04-13 23:18:48 +1200

    fixed sonme bugs

--- src/modals/DeleteModal.jsx
+++ src/modals/DeleteModal.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function DeleteModal({ type, title, onDeleteBtnClick }) {
   return (
     // Modal Container
-    <div className="fixed right-0 botton-0 left-0 top-0 px-2 py-4 overflow-x-scroll scrollbar-hide z-50 justify-center items-center flex bg-[#00000080] ">
+    <div className="fixed right-0 bottom-0 left-0 top-0 px-2 py-4 overflow-x-scroll scrollbar-hide z-50 justify-center items-center flex bg-[#00000080] ">
       {/* Delete Modal */}
       <div className="scrollbar-hide overflow-x-scroll max-h-[95vh] my-auto bg-white dark:bg-[#2b2c37] text-black dark:text-white w-full px-8 py-8 rounded-xl">
         <h3 className="font-bold text-red-500 text-lg">Delete this {type}</h3>
