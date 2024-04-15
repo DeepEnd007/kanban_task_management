71d89ecbe285c7ee0b617413f64099829c379670
DeepEnd007 <yihuihu56@gmail.com>
2024-04-15 14:36:37 +1200

    complete Subtask.jsx, start to work on TaskModal.jsx

---
+++ src/modals/TaskModal.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import elipsis from "../assets/icon-vertical-ellipsis.svg";
+import ElipsisMenu from "../components/ElipsisMenu";
+import Subtask from "../components/Subtask";
+import boardsSlice from "../redux/boardsSlice";
+
+function TaskModal({ colIndex, taskIndex, setIsTaskModalOpen }) {
+  const dispatch = useDispatch();
+  const boards = useSelector((state) => state.boards);
+  const board = boards.find((board) => board.isActive);
+  const columns = board.columns;
+  const col = columns.find((column, i) => colIndex === i);
+  const task = col.tasks.find((col, i) => taskIndex === i);
+
+  const subtasks = task.subtasks;
+
+  let completed = 0;
+  subtasks.forEach((subtask) => {
+    if (subtask.isCompleted) {
+      completed++;
+    }
+  });
+
+  const [status, setStatus] = useState(task.status);
+  const [newColIndex, setNewColIndex] = useState(columns.indexOf(col));
+  const [elipsisMenuOpen, setElipsisMenuOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  const setOpenEditModal = () => {
+    //write this function later
+  };
+  const setOpenDeleteModal = () => {
+    //write this function later
+  };
+
+  const onClose = (e) => {
+    if (e.target !== e.currentTarget) {
+      return;
+    }
+    dispatch(
+      boardsSlice.actions.setTaskStatus({
+        taskIndex,
+        colIndex,
+        newColIndex,
+        status,
+      })
+    );
+    setIsTaskModalOpen(false);
+  };
+  const onChange = () => {
+    setStatus(e.target.value);
+    setNewColIndex(e.target.selectedIndex);
+  };
+
+  return (
+    <div
+      onClick={onClose}
+      className=" fixed right-0 top-0 px-2 py-4 overflow-scroll scrollbar-hide  z-50 left-0 bottom-0 justify-center items-center flex dropdown "
+    >
+      {/* Modal Section */}
+      <div
+        className="scrollbar-hide overflow-y-scroll max-h-[95vh] my-auto bg-white dark:bg-[#2b2c37] text-black dark:text-white font-bold 
+      shadow-[#364e7e1a] max-w-md mx-auto w-full px-8 py-8 rounded-xl"
+      >
+        <div className="relative flex justify-between w-full items-center">
+          <h1 className="text-lg">{task.title}</h1>
+          <img
+            src={elipsis}
+            onClick={() => {
+              setElipsisMenuOpen((state) => !state);
+            }}
+            className="cursor-pointer h-6"
+          />
+          {elipsisMenuOpen && (
+            <ElipsisMenu
+              setOpenEditModal={setOpenEditModal}
+              setOpenDeleteModal={setOpenDeleteModal}
+              type="Task"
+            />
+          )}
+        </div>
+        <p className="text-gray-500 font-semibold tracking-wide text-sm pt-6">
+          {task.description}
+        </p>
+        <p className="pt-6 text-gray-500 tracking-widest text-sm">
+          Subtasks({completed} of {subtasks.length})
+        </p>
+        {/* Subtasks Section */}
+        <div className="mt-3 space-y-2">
+          {subtasks.map((subtask, i) => {
+            return (
+              <Subtask
+                index={i}
+                taskIndex={taskIndex}
+                colIndex={colIndex}
+                key={i}
+              />
+            );
+          })}
+        </div>
+        {/* Current Status Section */}
+        <div className="mt-8 flex flex-col space-y-3">
+          <label className="text-sm dark:text-white text-gray-500">
+            Current Status
+          </label>
+          <select
+            className="select-status flex-grow px-4 py-2 rounded-md text-sm
+          bg-transparent focus:board-0 border border-gray-300 focus:outline-[#635fc7] outline-none"
+            value={status}
+            onChange={onChange}
+          >
+            {columns.map((column, index) => (
+              <option className="status-option">{column.name}</option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TaskModal;
